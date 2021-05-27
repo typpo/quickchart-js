ab65af16f2904bae94cba8e1ef90c36c12cd9a2f
Brian Underwood <brian@brian-underwood.codes>
2021-05-27 14:05:29 +0200

    Support self-hosted usage
    Use `this.baseUrl` to allow for querying self-hosted versions of quickchart

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ class QuickChart {
       throw new Error('You must call setConfig before getUrl');
     }
 
-    const resp = await axios.post('https://quickchart.io/chart/create', this.getPostData());
+    const resp = await axios.post(`${this.baseUrl}/chart/create`, this.getPostData());
     if (resp.status !== 200) {
       throw `Bad response code ${resp.status} from chart shorturl endpoint`;
     } else if (!resp.data.success) {
@@ -116,7 +116,7 @@ class QuickChart {
       throw new Error('You must call setConfig before getUrl');
     }
 
-    const resp = await axios.post('https://quickchart.io/chart', this.getPostData(), {
+    const resp = await axios.post(`${this.baseUrl}/chart`, this.getPostData(), {
       responseType: 'arraybuffer',
     });
     if (resp.status !== 200) {
